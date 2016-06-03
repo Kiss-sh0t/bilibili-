17ffacf4fc7d10b10e4b8da5ed88da9ee4cfa274
Kiss-sh0t <1287651077@qq.com>
2016-06-03 09:41:29 +0800

    完整弹幕发送
    弹幕发送的时间轴确定

--- bilibili.py
+++ bilibili.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 import requests
 import Image
 import rsa
@@ -13,7 +14,7 @@ session = requests.Session()
 captcha = session.get('http://www.bilibili.com')
 c = captcha.cookies
 captcha = session.get('http://passport.bilibili.com/login', cookies=c)
-print captcha.cookies
+#print captcha.cookies
 captcha = session.get('http://passport.bilibili.com/captcha')
 sessionCookies = captcha.cookies
 
@@ -25,9 +26,10 @@ im.show()
 
 jsonText = session.get('http://passport.bilibili.com/login?act=getkey', cookies=sessionCookies)
 token = json.loads(jsonText.content)
-print jsonText.content
-username = 'user'
-passwd = 'pass'
+#print jsonText.content
+
+username = 'xxx'
+passwd = 'xxx'
 vdcode = raw_input('Enter captcha > ')
 def encryptpwd(passwd,token):
     password = token['hash'] + passwd
@@ -50,6 +52,7 @@ headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
 }
 jsonText = session.post('http://passport.bilibili.com/login/dologin', postdata, cookies=sessionCookies)
+#print jsonText.content
 
 #获取av信息
 av = 4757328
@@ -59,18 +62,24 @@ cid = m.group()[4:]
 t = time.strftime('%Y-%m-%d %H:%M:%S')
 
 #aid为视频的av号
-def postdanmu(content, t, pt): 
+#弹幕内容，当前时间和视频时间，视频时间以秒记，比如在1分20，时间就是80.00
+#字号有两种，一种18，一种25
+def postdanmu(content, t, pt, fsize=25): 
     postdata = {
-        'mode':0, #0为顶端弹幕，如果你要发送顶端的，改成1
+        'mode':1, #0为顶端弹幕，1为滚动字幕，2为低端渐隐，3为逆向滚动
         'color':'16777215', #还没测试其他颜色，先用白的试试吧
         'message': content, #弹幕内容
         'pool':'0', #这个字段什么鬼
-        'playTime': pt, #在视频中的时间轴
+        'playTime': pt, #在视频中的时间轴，单位为秒，小数点后两位
         'cid': cid, 
-        'fontsize': 25, #字体大小，两种规格，这个是那个小的
+        'fontsize': fsize, #字体大小，两种规格，这个是那个小的
         'rnd': 1485739224, #好像是个时间参数，蛋疼啊，到底怎么来的，算了，随便生成一个好了_(:з」∠)_
         'data': str(t) 
     }
     return session.post('http://interface.bilibili.com/dmpost?cid='+str(cid)+'&aid='+str(av)+'&pid=1', postdata)
-#这里是测试，批量发送再说
-test = postdanmu('hahaha', t, 0)
+
+#测试，可以有中文
+test = postdanmu('maybe the first one?', t, 80.08)
+
+#如果不是-1，发送成功
+print test.content
